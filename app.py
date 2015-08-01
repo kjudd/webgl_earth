cecb4d0ba29b1f0660b9f2853234bcb5a91c8a3a
kjudd <vedette.kristen@gmail.com>
2015-08-01 09:14:28 -0700

    File cleanup, added requirements.txt for deployment to Heroku

--- app.py
+++ app.py
@@ -2,12 +2,8 @@ from flask import Flask, render_template, redirect
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world1():
-    return render_template('wheel1.html')
-
-@app.route('/wheel')
 def hello_world():
-    return render_template('wheel.html')
+    return render_template('earth.html')
 
 if __name__ == '__main__':
     app.run()
